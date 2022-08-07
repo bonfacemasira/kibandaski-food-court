ed023e0e2115db5907409340043124ee62f6bc65
Bonface Masira <bonfacemasira@gmail.com>
2022-08-07 07:36:08 +0300

    fetch list of recipes ad store in state

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import LatestRecipes from "./LatestRecipes";
 import Login from "./Login";
 import MorePopularRecipes from "./MorePopularRecipes";
 import Pagination from "./Pagination";
+import axios from "axios";
 
 function Main() {
+
+    const [meals, setMeals] = useState([]);
+
+    useEffect(() => {
+        const options = {
+            method: 'GET',
+            url: 'https://tasty.p.rapidapi.com/recipes/list',
+            params: {from: '0', size: '20', tags: 'under_30_minutes'},
+            headers: {
+              'X-RapidAPI-Key': '4528166eb9msheffa2c87ee71260p17ada3jsnd19d285afb10',
+              'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+            }
+          };
+          
+          axios.request(options).then(function (response) {
+            setMeals(response.data);
+          }).catch(function (error) {
+            console.error(error);
+          });
+    }, [])
+
+    console.log(meals)
+    
     return(
         <div>
             <Login />
